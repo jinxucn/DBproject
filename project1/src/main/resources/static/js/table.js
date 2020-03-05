d6078c2264d85775050b647c5dbf91e77ee0ea74
Jin <jin.xucn@outlook.com>
2020-03-04 20:19:35 -0500

    add schema and history

--- project1/src/main/resources/static/js/table.js
+++ project1/src/main/resources/static/js/table.js
@@ -1,17 +1,61 @@
 /*
  * @Author: Jin X
  * @Date: 2020-02-17 23:58:08
- * @LastEditTime: 2020-03-04 11:33:53
+ * @LastEditTime: 2020-03-04 19:47:35
  */
-var history = []
+var history = {}
 $(function () {
     var $tb = $('#tb');
+
+
+    var hisid=0
+
+    function addHistory() {
+        let form = $('#fm').serializeArray();
+        let type = form[0]['value'];
+        let sql = form[1]['value'];
+        let time = $('#time').text();
+        // let type='mysql'
+        // let sql ='select * from aisles;'
+        // let time='84ms'
+        let tb = $('.bootstrap-table').clone();
+        tb.find('#colh,#tb').removeAttr('id');
+        tb.addClass('history-tb');
+        // let tb=$tb.clone().removeAttr('id').addClass('history-tb')
+        let ha = $('<a></a>').text('history' + (++hisid)).addClass('dropdown-item').attr('href', '#');
+        history[ha.text()] = tb;
+        $('.dropdown-menu').append(ha)
+        ha.click(function () {
+            $('.nav-link').removeClass('active')
+            $('#page-history').addClass('active')
+            $('.sql-type').text(type);
+            $('.time').text(time);
+            $('.history-sql').text(sql)
+            $('.history-tb').remove();
+            $('#history').append(tb[0]);
+            $('#query').hide();
+            $('#schema').hide();
+            $('#history').show();
+        })
+
+    };
+
+
+
+
+
+
+
+
+
+
+
     $("#submit").click(function () {
         $tb.bootstrapTable('showLoading');
         var timeBegin = new Date();
         $.ajax({
             type: "POST",
-            timeout: 30000,
+            timeout: 10000,
             dataType:"json",
             url: "/sql/e",
             data: $('#fm').serializeArray(),
@@ -30,6 +74,7 @@ $(function () {
                 var timeEnd = new Date();
                 $('#time').text((timeEnd-timeBegin)+'ms')
                 console.log('load data')
+                addHistory()
             },
             error: function () {
                 alert("error");
